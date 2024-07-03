a16c968907aa5b84bca8386e5b5f5ecde6c5c320
0xSmitty <badcoins4me@gmail.com>
2024-07-03 17:06:57 +0100

    capital Fling

--- src/app/Components/utils/Twitter/TwitterIntent.tsx
+++ src/app/Components/utils/Twitter/TwitterIntent.tsx
@@ -7,7 +7,7 @@ export default function TwitterIntent(){
         <div className='w-full h-auto flex flex-col justify-start items-center'>
         <button 
             className="w-[200px] bg-[#4d619e] p-2 border-2 border-solid border-white text-[#f3f3f3] font-text text-xl rounded-2xl hover:bg-pink-300 shadow-2xl flex flex-col items-center"
-            onClick={() => window.open("https://twitter.com/intent/tweet?url=https://house.humpingunicorns.com/&text=I%20just%20sent%20my%20Unicorns%20to%20the%20Hump%20House%21%0A%0AHopefully%20they're%20lucky%20enough%20to%20have%20a%20fling%20and%20bring%20home%20a%20friend!", "_blank")}
+            onClick={() => window.open("https://twitter.com/intent/tweet?url=https://house.humpingunicorns.com/&text=I%20just%20sent%20my%20Unicorns%20to%20the%20Hump%20House%21%0A%0AHopefully%20they're%20lucky%20enough%20to%20have%20a%20Fling%20and%20bring%20home%20a%20friend!", "_blank")}
         >
             <img 
                 src="/humping_images/Tweet_About_It.gif"
